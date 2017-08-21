bbe4a7549ef2663046c2c191ebf5c91ff970e7ff
zouqianli <1004248149@qq.com>
2017-08-21 09:09:40 +0800

    2. 生活服务分类管理模块 2.3.1 优化 添加一级分类; 控制器调用模型的add()方法,对成功\失败设置提示消息; 模型使用自动添加\更新时间戳(database配置auto_timestamp=true);

--- imooc_o2o/application/common/model/Category.php
+++ imooc_o2o/application/common/model/Category.php
@@ -5,14 +5,19 @@ use think\Model;
 class Category extends Model
 {
     /**
+     * @var bool自动写入插入时间和更新时间
+     * 还可以通过database.php配置文件设置auto_timestamp=true
+     */
+    protected $autoWriteTimestamp = true;
+
+    /**
      * @param $data 添加分类数据
      * @return false|int 保存成功\失败
      */
     public function add($data)
     {
         $data['status'] = 1; // 默认分类状态:正常
-        $data['parent_id'] = 0; // 默认分类为一级分类:父类id=0
-        $data['create_time'] = time(); // 默认分类添加时间:当前时间
+        //$data['create_time'] = time(); // 默认分类添加时间:当前时间
         return $this->save($data); // 保存\修改
     }
 }
